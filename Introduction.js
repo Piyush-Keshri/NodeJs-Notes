99085f73d75810ff17558c3e96705d59490a0d75
Piyush Keshri <piyushkeshri383@gmail.com>
2022-03-03 16:05:17 +0530

    commit

---
+++ Introduction.js
@@ -0,0 +1,22 @@
+// What does Node.js do?
+/*
+* Node.js is a javaScript Runtime built on google's open source V8 JavaScript Engine.
+* Node.js is just like a container,like an enviornment,in which a program written in JavaScript can be executed but outside of any browser whatsoever.
+* In Node.js standalone enviornment we can do many more things with JavaScript like accessing file systems or better network capabilities.
+* Using Node.js we can use JavaScript on the server side of web development to build fast ,highly scalable network applications(back-end).
+
+// Why to use Node.js
+
+* Node.js is single-threaded ,based on event driven,non-blocking I/O model which makes NodeJs very lightweight and efficient.
+* Node.js is perfect for building fast and scalable data-intensive web applications.
+* Companies like NETFLIX,UBER,PAYPAL,EBAY have started using Node in production.
+* Very Active developer community.
+
+// When to use Node.js
+
+* To bulid APIs with Database behind it(preferably NoSQL).
+* For Data Streaming(Eg: Youtube);
+* Real-time Chat Application.
+* Server-Side Web Application. 
+
+*/
