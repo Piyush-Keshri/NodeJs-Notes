97a097167ab9cca004375e67068bb7252ad35fb1
Piyush Keshri <piyushkeshri383@gmail.com>
2022-03-15 10:32:53 +0530

    commit

--- Asynchronous-JS/index.js
+++ Asynchronous-JS/index.js
@@ -1,5 +1,32 @@
+const { rejects } = require('assert');
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
+
+const readFilePro = file => {
+  return new Promise((resolve,reject) => {
+    fs.readFile(file,(err,data) =>{
+      if(err) reject('Could not find that file 😎');
+      resolve(data);
+    });
+  });
+}
+
+readFilePro(`${__dirname}/dog.txt`).then(data => {
+  console.log(`Breed : ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
+    })
+    .catch((err) => console.log(err));
+});
 
 // CallBack Hell
 
@@ -33,18 +60,3 @@ The fullfilled Promises are handled by the then() method and rejected promises a
 
 */
 
-// Using then()
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed : ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file!');
-      });
-    }).catch(err => console.log(err));
-});
---
+++ express.js
@@ -0,0 +1,12 @@
+/*
+* What is Express?
+-- Express is a minimal Node.js framework which means that it is actually built on top of Node.js providing higher level of Abstraction.
+-- It is the most popular Node.js Framework and is written 100% in Node.js code.
+-- 
+
+* Why should we use Express?
+-- Express contains a very Robust set of features like complex routing,easier handling of requests and responses,middleware,server side rendering etc.
+-- Express Allows for rapid Development of Node.js Applications.
+-- Express makes it easier to organise our application into the MVC architecture.
+
+*/
