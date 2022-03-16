027cec4a998e7e652179f55aceb1339c5cc0fc71
Piyush Keshri <piyushkeshri383@gmail.com>
2022-03-16 20:42:08 +0530

    Commit

---
+++ api.js
@@ -0,0 +1,24 @@
+/*
+* API -- Application Programming Interface.
+
+APIs are piece of software that can be used by another piece of software in order to allow applications to talk to each other.
+
+//----------------------- THE REST ARCHITECTURE ------------------//
+
+REST -- Representational States Transfer.
+
+It is basically a way of building web APIs in a logical way,making them easy to consume.
+
+To Build APIs following the REST Architecture following Principles are Followed :
+
+1. Separate API into Logical resources.
+2. Expose structured,resource-based URLs
+3. Use HTTP methods -- Post,Get,Put,Patc,Delete.
+4. Send Data as JSON
+5. Be stateless.
+
+Stateless Restful API -- All state is handled on the client.This means that each request must contain all the information necessary
+                        to process a certain request. The server should not have to remember previous requests.
+Examples of state -- loggedIn ,currentPage.                        
+
+*/
